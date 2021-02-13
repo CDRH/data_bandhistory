629e586b959313aca0232f69891438af00f69d6d
Jessica Dussault <jdussault4@gmail.com>
2021-02-13 16:35:22 -0600

    updates csvs and authority from spreadsheets

--- scripts/overrides/tei_to_es.rb
+++ scripts/overrides/tei_to_es.rb
@@ -1,5 +1,12 @@
 class TeiToEs < XmlToEs
 
+  def override_xpaths
+    {
+      "person" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='people']/term",
+      "publisher" => "//sourceDesc/msDesc/msIdentifier/repository"
+    }
+  end
+
   def image_id
     img = get_list(@xpaths["image_id"]).first
     "#{img}.jpg"
