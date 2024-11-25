bd3b084ee85bb8cbdc712cf989fc192d27f5a623
nichgray <ngray2@unl.edu>
2024-11-25 13:24:19 -0600

    add collection, remove publisher path

--- scripts/overrides/tei_to_es.rb
+++ scripts/overrides/tei_to_es.rb
@@ -3,13 +3,17 @@ class TeiToEs < XmlToEs
   def override_xpaths
     {
       "person" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='people']/term",
-      "publisher" => "//sourceDesc/msDesc/msIdentifier/repository"
+      "collection" => "//sourceDesc/msDesc/msIdentifier/collection"
     }
   end
 
   def cover_image
     img = get_list(@xpaths["image_id"]).first
     "#{img}.jpg"
+  end
+
+  def rights_uri
+    get_text(@xpaths["collection"])
   end
 
   def spatial
